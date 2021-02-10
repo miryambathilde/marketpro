c2947e097ad9c2d273e0a0377a48529f19ca4a43
Miryam Bathilde <mbathilde@gmail.com>
2021-02-10 13:52:55 +0100

    nav bar menu + titulos dinamicos + archivos php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!--INCLUDERS -->
+<?php 
+$tituloPagina = "Inicio MarketPro";
+include('inc/header.php'); 
+?>
+
+
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron">
+      <div class="container">
+        <h1>Bienvenidos a MarketPro </h1>
+        <p>Tu tienda online de confianza donde encontrarás los mejores productos al mejor precio.</p>
+        <p><a class="btn btn-primary btn-lg" href="#" role="button">Ver más &raquo;</a></p>
+      </div>
+    </div>
+
+<?php include('inc/footer.php'); ?>   
