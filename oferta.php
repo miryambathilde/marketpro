c677ab8d1a5f8cd76dd07ee637f0b2c9e2fa9d7d
Miryam Bathilde <mbathilde@gmail.com>
2021-02-11 13:58:56 +0100

    agregando vista simple de cada oferta

---
+++ oferta.php
@@ -0,0 +1,23 @@
+<!--aqui cargamos el archivo de funciones.php -->
+<?php include('inc/funciones.php'); ?>
+
+<!--INCLUDERS -->
+<?php 
+$tituloPagina = "Oferta Destacada";
+$pagina = "oferta";
+include('inc/header.php'); 
+?>
+
+
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron">
+      <div class="container">
+        <h1>Bienvenidos a MarketPro </h1>
+        <p>Tu tienda online de confianza donde encontrarás los mejores productos al mejor precio.</p>
+        <p><a class="btn btn-primary btn-lg" href="#" role="button">Ver más &raquo;</a></p>
+      </div>
+    </div>
+
+   
+
+<?php include('inc/footer.php'); ?>   
