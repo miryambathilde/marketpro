a7ab4e95f14495a694afea05cf4a36146cac2718
Miryam Bathilde <mbathilde@gmail.com>
2021-02-11 14:36:09 +0100

    vista de oferta por id y redirect to ofertas.php

--- oferta.php
+++ oferta.php
@@ -3,6 +3,16 @@
 
 <!--INCLUDERS -->
 <?php 
+if (isset($_GET ["id"])) {
+  $oferta_id = $_GET["id"];
+if (isset($ofertas[$oferta_id])){
+$oferta = $ofertas[$oferta_id];
+}
+} else {
+  header("Location: ofertas.php");
+  exit();
+}
+
 $tituloPagina = "Oferta Destacada";
 $pagina = "oferta";
 include('inc/header.php'); 
@@ -12,9 +22,18 @@ include('inc/header.php');
     <!-- Main jumbotron for a primary marketing message or call to action -->
     <div class="jumbotron">
       <div class="container">
-        <h1>Bienvenidos a MarketPro </h1>
-        <p>Tu tienda online de confianza donde encontrarás los mejores productos al mejor precio.</p>
-        <p><a class="btn btn-primary btn-lg" href="#" role="button">Ver más &raquo;</a></p>
+        <div class="row">
+        <!-- primera columna donde voy a mostrar la imagen, el nombre y la descripcion-->
+        <div class="col-md-8">
+          <p> <img src="<?php echo $oferta["imagen"]; ?>" alt= "<?php echo $oferta["nombre"];?>"></p>
+        <h2><?php echo $oferta["nombre"]; ?></h2>
+        <p><?php echo $oferta["descripcion"]; ?></p>
+      </div>
+
+      <!-- columna a la derecha con el boton de compra  -->
+      <div class="col-md-4">
+      <p><a class="btn btn-danger" href="#">Comprar: Antes <?php echo $oferta["precio"]; ?> <strong>Ahora <?php echo $oferta["precioOferta"]; ?></strong></a></p>
+      </div>
       </div>
     </div>
 
